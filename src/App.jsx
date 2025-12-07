7036df7df06a4768b95b7d1e787acb1c80b63aed
ankitverma3490 <ankitverma3490@gmail.com>
2025-12-07 13:29:12 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 
 import React from "react";
 import BookingFlow from "./pages/BookingFlow";
+import Admindashboard from "./admin/AdminDashboard"
 import "./App.css";
 
-const App = () => <BookingFlow />;
+const App = () => 
+<Admindashboard/>
+{/* <BookingFlow /> */}
+;
 
 export default App;
